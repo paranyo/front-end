81d83444ad5e2e1fa82a45686b12c72447891b71
paranyo <gksrbxo123@gmail.com>
2024-06-10 19:16:16 +0900

    vaas

--- next.config.js
+++ next.config.js
@@ -6,23 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://api.mooluck.xyz/:path*',
-          }, // replace this your actual origin
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-        destination: 'https://api.mooluck.xyz/:path*',
+        destination: 'https://api.mooluck.xyz/api/:path*',
         permanent: true,
       },
     ];
