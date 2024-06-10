9a0546181623dabdd1670bdd05c2553ca6631c1b
paranyo <gksrbxo123@gmail.com>
2024-06-10 19:12:54 +0900

    vaas

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
         source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://api.mooluck.xyz/:path*',
+          }, // replace this your actual origin
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT',
