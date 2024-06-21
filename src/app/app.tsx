d1a799c9a000ef34cfbe5158d764046faf6ee338
paranyo <gksrbxo123@gmail.com>
2024-06-21 20:11:29 +0900

    Asd

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,6 +4,7 @@ import { Toaster } from 'react-hot-toast'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Cart from './cart'
 import { StrictMode } from 'react'
+import Delivery from './delivery'
 
 export function App() {
   return (
@@ -13,6 +14,7 @@ export function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/cart" element={<Cart />} />
+            <Route path="/delivery" element={<Delivery />} />
             <Route path="/" element={<Main title={'mooluck'} />} />
           </Routes>
         </BrowserRouter>
---
+++ src/app/delivery.tsx
@@ -0,0 +1,71 @@
+import { Box, Image, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+
+export function Delivery() {
+  return (
+    <Box maxW="400px" m="0 auto" border="1px solid black">
+      <Tabs>
+        <TabList>
+          <Tab>아이스크림&과자</Tab>
+          <Tab>냉동식품</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_1.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_2.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_3.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_4.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_5.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_6.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_7.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_8.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_9.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_10.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_11.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_12.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_13.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_14.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_15.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_16.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/bobki/bobki_17.jpeg" />
+          </TabPanel>
+          <TabPanel>
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_1.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_2.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_3.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_4.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_5.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_6.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_7.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_8.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_9.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_10.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_11.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_12.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_13.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_14.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_15.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_16.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_17.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_18.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_19.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_20.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_21.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_22.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_23.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_24.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_25.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_26.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_27.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_28.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_29.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_30.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_31.jpeg" />
+            <Image mb={2} src="https://soonroom.s3.ap-northeast-2.amazonaws.com/picture/icecream/icecream_32.jpeg" />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  )
+}
+
+export default Delivery;
